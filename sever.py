d6dc9fe74f1945e5d9d06ec6325f324b58566b23
khaint14 <khaint6847@ut.edu.vn>
2025-08-28 23:34:02 +0700

    them thu vien - du lieu can thiet

---
+++ sever.py
@@ -0,0 +1,16 @@
+import socket
+import json
+import re
+from datetime import datetime
+import uuid
+import threading
+
+# =====================
+# Dữ liệu chuyến chung
+# =====================
+trips = {
+    'BINH DINH -> HCM': {'total_seats': 20, 'booked_seats': {}},
+    'HCM -> BINH DINH': {'total_seats': 20, 'booked_seats': {}},
+    'DAK LAK -> HCM': {'total_seats': 20, 'booked_seats': {}},
+    'HCM -> DAK LAK': {'total_seats': 20, 'booked_seats': {}},
+}
